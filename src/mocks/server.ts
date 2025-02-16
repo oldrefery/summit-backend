db38e4b7d9c6c7efb5ff60267d2056d7e3d216ed
Rustem Shakirov <oldrefery@gmail.com>
2025-02-16 01:19:03 +0100

    refactor: remove all related to the attempt to add test to the project

--- src/mocks/server.ts
+++
@@ -1,6 +0,0 @@
-// src/mocks/server.ts
-import { setupServer } from 'msw/node'; // Импортируем setupServer из msw/node
-import { handlers } from './handlers'; // Импортируем моковые обработчики
-
-// Создаем моковый сервер с использованием обработчиков
-export const server = setupServer(...handlers);
